69c3c60a10fcea688710822fce10c2f82ff00434
Ayaka-Ikegami <user@anioMacBookPro.local>
2025-07-19 02:20:36 +0900

    [add]ログインと新規登録ページ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable
+
+  validates :user_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, confirmation: true, on: :create
 
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
